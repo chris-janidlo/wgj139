63cef0588e475b463b6d0ff5d7edc15c3ed2b6b5
crassSandwich <christopherjanidlo@gmail.com>
2020-03-06 22:57:14 -0500

    Bird visual

---
+++ Assets/Scripts/FlappingAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappingAnimation : MonoBehaviour
+{
+    public float SpeedModifier { get; set; } = 1;
+
+    public float FPS;
+    public List<Mesh> Frames;
+    public MeshFilter MeshFilter;
+
+    IEnumerator Start ()
+    {
+        int i = 0;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1 / (FPS * SpeedModifier));
+
+            MeshFilter.mesh = Frames[i];
+            i = (i + 1) % Frames.Count;
+        }
+    }
+}
